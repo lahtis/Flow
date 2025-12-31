f53f46ea5af5c15a1ccadf2ba31a57f8d59b4a10
Tuomas Lähteenmäki <lahtis@users.noreply.github.com>
2025-12-31 14:32:44 +0200

    Delete Ask Jeeves/test_gui.py

--- Ask Jeeves/test_gui.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as ctk
-
-app = ctk.CTk()
-app.geometry("400x240")
-app.title("Jeeves Test")
-label = ctk.CTkLabel(app, text="If you see this, the library is working, sir!")
-label.pack(pady=20)
-app.mainloop()
